f895ee1b13c7ebafc7941611daaa732c295131c7
yoo-ssup <123109949+yoo-ssup@users.noreply.github.com>
2023-01-29 22:40:07 +0530

    Added Cart Context, Shopping Icon and Cart Dropdown

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+import { CartProvider } from './contexts/cart.context';
 
 
 const container = document.getElementById('root');
@@ -16,7 +18,11 @@ root.render(
   <React.StrictMode>
      <BrowserRouter>
       <UserProvider>
-       <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
      </BrowserRouter>
    </React.StrictMode>
