3a09d0c5b4d2aa68c4858997026d3ce594e5d1b7
yoo-ssup <123109949+yoo-ssup@users.noreply.github.com>
2023-02-04 01:17:49 +0530

    Final Phase

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
-import { CartProvider } from './contexts/cart.context';
 
 
 const container = document.getElementById('root');
@@ -18,11 +18,11 @@ root.render(
   <React.StrictMode>
      <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
      </BrowserRouter>
    </React.StrictMode>
