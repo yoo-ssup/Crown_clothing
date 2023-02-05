9c882dcd717411d20339460866ee6139c02f74ca
yoo-ssup <123109949+yoo-ssup@users.noreply.github.com>
2023-02-05 21:28:35 +0530

    Added reducers in place of useState and useEffect in Context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,26 +1,58 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from 'react';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth, signOutUser } from "../utils/firebase/firebase.utils";
+import { createAction } from '../utils/reducer/reducer.utils';
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
   currentUser: null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null); //Actual Storage
-  const value = { currentUser, setCurrentUser };
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
 
-  useEffect(()=>{   //Call for the actual storage
-    const unsubscribe = onAuthStateChangedListener((user)=>{
-      if(user){
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+export const UserProvider = ({ children }) => {
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) =>
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
         createUserDocumentFromAuth(user);
       }
-      setCurrentUser(user); 
+      setCurrentUser(user);
     });
-  
+
     return unsubscribe;
   }, []);
+
+  console.log(currentUser);
+
+  const value = {
+    currentUser,
+  };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
