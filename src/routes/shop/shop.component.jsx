a429acf526cc4f322d9f519c760ee10871106d0c
yoo-ssup <123109949+yoo-ssup@users.noreply.github.com>
2023-02-23 00:22:13 +0530

    Handling asynchronous behaviour with Redux Thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,8 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/categories.action';
+import { fetchCategoriesAsync } from '../../store/categories/categories.action';
 
 import './shop.styles.scss'
 
@@ -13,12 +12,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-        const getCategoriesMap = async () =>{
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categoriesArray));
-        };
-        
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     },[dispatch]);
 
     return(
--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist"; 
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-// import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
